eac130295febc158d0be4d57f039ff004c2d7ea5
Quiatus <Quiatus@gmail.com>
2024-06-18 15:07:11 +0200

    add error handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,10 +4,6 @@ import recipeView from './views/recipeView.js'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
-const timeout = (s) => {
-  return new Promise((_, reject) => setTimeout(() => reject(new Error(`Request took too long! Timeout after ${s} second`)), s * 1000))
-};
-
 const controlRecipes = async () => {
   try {
     const id = window.location.hash.slice(1)
@@ -20,8 +16,12 @@ const controlRecipes = async () => {
 
     recipeView.render(model.state.recipe)
   } catch (err) {
-    alert(err)
+    recipeView.renderError()
   }
 }
 
-['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipes))
+const init = () => {
+  recipeView.addHandlerRender(controlRecipes)
+}
+
+init()
---
+++ src/js/helpers.js
@@ -0,0 +1,17 @@
+import { TIMEOUT_SECONDS } from './config.js'
+
+const timeout = (s) => {
+    return new Promise((_, reject) => setTimeout(() => reject(new Error(`Request took too long! Timeout after ${s} second`)), s * 1000))
+  };
+
+export const getJSON = async (url) => {
+    try {
+        const res = await Promise.race([fetch(url), timeout(TIMEOUT_SECONDS)])
+        const data = await res.json()
+    
+        if (!res.ok) throw new Error(`${data.message} ${res.status}`)
+        return data
+    } catch (err) {
+        throw err
+    }
+}
